84990ea730991e2f347acda48e7dfdedc728160a
Kain991 <kainarberkin03012003@gmail.com>
2021-10-26 02:37:29 +0600

    first commit

---
+++ cmake-build-debug/CMakeFiles/TicTacToeKainKami.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TicTacToeKainKami.dir/main.cpp.obj"
+  "TicTacToeKainKami.exe"
+  "TicTacToeKainKami.exe.manifest"
+  "TicTacToeKainKami.pdb"
+  "libTicTacToeKainKami.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TicTacToeKainKami.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <ctime>
+#include <cstdlib>
+#include <algorithm>
+using namespace std;
+class Environment {
+public:
+    Environment(){
+        for(int i =0; i < 3 ;i++){
+            for(int j = 0 ;j < 3 ;j++){
+                board[i][j] = ' ';
+            }
+        }
+    }
+    char board[3][3];
+    void drawboard();
+    char checkwinner();
+    bool checkturn(int y , int x);
+
+};
+class Player : public Environment{
+public:
+    void playwithhuman();
+    void playwithbot();
+};
+
+int main() {
+    Player player;
+
+    int choice;
+    while(choice != 4){
+        cout<<"1.Player vs. Player\n"
+            <<"2.Player vs. Bot\n"
+            <<"3.Player vs. AdvBot\n"
+            <<"4.Quit\n";
+        cin>>choice;
+        if(choice == 1){
+            player.playwithhuman();
+        }
+        else if(choice == 2){
+            player.playwithbot();
+        }
+        else if(choice == 3){
+
+        }
+        else {
+            break;
+        }
+
+    }
+}
